aa0473b68504ae6ba74db9e3bf7b0456126dd2ac
ritesh sanger <riteshsengar26@gmail.com>
2019-07-26 00:58:04 +0530

    second commit

---
+++ crawl.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+async function run() {
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://github.com/');
+
+  //browser.close();
+}
+
+run();
---
+++ creds.js
@@ -0,0 +1,4 @@
+module.exports = {
+    username: 'ritesh sanger',
+    password: 'mindcrew01'
+}
