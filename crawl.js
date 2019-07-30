d2ebba590bccbc92d2227bfbc11d53611737b400
ritesh sanger <riteshsengar26@gmail.com>
2019-07-31 05:26:23 +0530

    user info and email

--- crawl.js
+++ crawl.js
@@ -28,7 +28,8 @@ async function run() {
 
   await page.click(BUTTON_SELECTOR);
 
-  //await page.waitForNavigation();
+
+  await page.waitFor(10*1000);
 
   //await page.screenshot({ path: '/home/rudra/Pictures/screenshots/login-github.png' });
   const userToSearch = 'john';
@@ -41,13 +42,12 @@ async function run() {
  // const LIST_USERNAME_SELECTOR = '#user_search_results > div.user-list > div:nth-child(1) > div.d-flex > div > a';
   const LIST_USERNAME_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > a';
 // const LIST_EMAIL_SELECTOR = '#user_search_results > div.user-list > div:nth-child(2) > div.d-flex > div > ul > li:nth-child(2) > a';
-  const LIST_EMAIL_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > ul > li:nth-child(2) > a';
+ const LIST_EMAIL_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > ul > li:nth-child(2) > a';
 
  const LIST_USERNAME_BUTTON_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > a';
   
- 
- const LENGTH_SELECTOR_CLASS = 'user-list-item';
-
+  const LENGTH_SELECTOR_CLASS = 'user-list-item';
+  const Info_Selector = '#js-pjax-container > div > div.h-card.col-lg-3.col-md-4.col-12.float-md-left.pr-md-3.pr-xl-6 > div.clearfix.mb-2 > div.float-left.col-9.col-md-12.pl-2.pl-md-0 > div:nth-child(5) > div > div > div';
  // extract text from element by using evaluate method..
 
   let listLength = await page.evaluate((sel) => {
@@ -61,34 +61,48 @@ async function run() {
 for (let i = 1;i<= listLength ; i++){
 
   
-  let usernameButtonSelector = LIST_USERNAME_BUTTON_SELECTOR.replace("INDEX",i);
-  await page.click(usernameButtonSelector);
-  await page.waitFor(5*1000);
-  await page.goBack();
-  await page.waitFor(5*1000);
-
-  /*
   let usernameSelector = LIST_USERNAME_SELECTOR.replace("INDEX",i);
   let emailSelector = LIST_EMAIL_SELECTOR.replace("INDEX",i);
   //now we extract username from usernameSelector
   let username = await page.evaluate((sel) => {
     return document.querySelector(sel).getAttribute('href').replace('/', '');
   }, usernameSelector);
- 
-  // extract email from emailSelector
+
   let email = await page.evaluate((sel) => {
     let element = document.querySelector(sel);
-    
-    // checking if email not present or visible
-    return element? element.innerHTML: null;
-  }, emailSelector);
 
-  //not all user have email visible
+    return element? element.innerHTML: null;
+  },emailSelector);
+ 
+    //await page.click(usernameSelector);
+ // await page.waitFor(2*1000);
+
+
+ let usernameButtonSelector = LIST_USERNAME_BUTTON_SELECTOR.replace("INDEX",i);
+ await page.click(usernameButtonSelector);
+ await page.waitFor(3*1000);
+  
+
+  
+  let info = await page.evaluate((sel) => {
+    let element = document.querySelector(sel);
+        // checking if email not present or visible
+    return element? element.innerHTML: null;
+  }, Info_Selector);
+
+
+
+  console.log(username,'==>>',info,'=>',email);
+  //console.log(username, ' -> ', email);
+  await page.goBack();
+  await page.waitFor(3*1000);
+
+  
+
+  //not all user have email visible 
  // if (!email)
      // continue;
 
-    console.log(username, ' -> ', email);
-*/
 
 }
 
